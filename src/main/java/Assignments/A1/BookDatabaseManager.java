11cc350e202d7bdf772a9235da3904d2610fcfa3
Parker Wallace <wallace.parker@hotmail.com>
2025-02-03 15:08:18 -0330

    added fucntionality for resetting db

--- src/main/java/Assignments/A1/BookDatabaseManager.java
+++ src/main/java/Assignments/A1/BookDatabaseManager.java
@@ -66,8 +66,14 @@ public class BookDatabaseManager {
         }
     }
 
-    public void updateDatabase(String table, String column, String updatedValue, String oldValueColumn, String oldValue) throws SQLException {
-        String Query = "Update %s set %s = %s where %s = %s";
+    public void updateDatabase(String table,
+                               String column,
+                               String updatedValue,
+                               String oldValuePrimaryKeyColunmName,
+                               String oldValuePrimaryKey) throws SQLException {
+        String Query = String.format("Update %s set %s = '%s' where %s = '%s'",
+                table, column, updatedValue, oldValuePrimaryKeyColunmName, oldValuePrimaryKey);
+        executeQuery(Query);
     }
 
     public void addBook(Book book) {}
@@ -126,5 +132,67 @@ public void executeQuery(String Query) {
     }
 }
 
+public void resetDatabase() {
+    List<String> Queries = new ArrayList<>();
+    Queries.add("DROP TABLE IF EXISTS authorISBN");
+    Queries.add("DROP TABLE IF EXISTS titles");
+    Queries.add("DROP TABLE IF EXISTS authors");
+    Queries.add("""
+            CREATE TABLE authors (
+               authorID INT NOT NULL AUTO_INCREMENT,
+               firstName varchar (20) NOT NULL,
+               lastName varchar (30) NOT NULL,
+               PRIMARY KEY (authorID)
+            );""");
+    Queries.add("""
+            CREATE TABLE titles (
+               isbn varchar (20) NOT NULL,
+               title varchar (100) NOT NULL,
+               editionNumber INT NOT NULL,
+               copyright varchar (4) NOT NULL,
+               PRIMARY KEY (isbn));""");
+    Queries.add("""
+            CREATE TABLE authorISBN (
+               authorID INT NOT NULL,
+               isbn varchar (20) NOT NULL,
+               FOREIGN KEY (authorID) REFERENCES authors (authorID),\s
+               FOREIGN KEY (isbn) REFERENCES titles (isbn));""");
+    Queries.add("""
+            INSERT INTO authors (firstName, lastName)
+            VALUES\s
+               ('Paul','Deitel'),\s
+               ('Harvey','Deitel'),
+               ('Abbey','Deitel'),\s
+               ('Dan','Quirk'),
+               ('Michael','Morgano');""");
+    Queries.add("""
+            INSERT INTO titles (isbn,title,editionNumber,copyright)
+            VALUES
+               ('0132151006','Internet & World Wide Web How to Program',5,'2012'),
+               ('0133807800','Java How to Program',10,'2015'),
+               ('0132575655','Java How to Program, Late Objects Version',10,'2015'),
+               ('013299044X','C How to Program',7,'2013'),\s
+               ('0132990601','Simply Visual Basic 2010',4,'2013'),
+               ('0133406954','Visual Basic 2012 How to Program',6,'2014'),
+               ('0133379337','Visual C# 2012 How to Program',5,'2014'),
+               ('0136151574','Visual C++ How to Program',2,'2008'),
+               ('0133378713','C++ How to Program',9,'2014'),
+               ('0133764036','Android How to Program',2,'2015'),
+               ('0133570924','Android for Programmers: An App-Driven Approach, Volume 1',2,'2014'),
+               ('0132121360','Android for Programmers: An App-Driven Approach',1,'2012');""");
+    Queries.add("""
+            INSERT INTO authorISBN (authorID,isbn)
+            VALUES
+               (1,'0132151006'),(2,'0132151006'),(3,'0132151006'),(1,'0133807800'),(2,'0133807800'),(1,'0132575655'),
+               (2,'0132575655'),(1,'013299044X'),(2,'013299044X'),(1,'0132990601'),(2,'0132990601'),(3,'0132990601'),
+               (1,'0133406954'),(2,'0133406954'),(3,'0133406954'),(1,'0133379337'),(2,'0133379337'),(1,'0136151574'),
+               (2,'0136151574'),(4,'0136151574'),(1,'0133378713'),(2,'0133378713'),(1,'0133764036'),(2,'0133764036'),
+               (3,'0133764036'),(1,'0133570924'),(2,'0133570924'),(3,'0133570924'),(1,'0132121360'),(2,'0132121360'),
+               (3,'0132121360'),(5,'0132121360');""");
+
+    for (String query : Queries) {
+        executeQuery(query);
+    }
+}
 
 }
