09b9997992ea9b7c9b1bf68b1a67f76baf954faa
parker wallace <wallace.parker@hotmail.com>
2025-02-13 14:51:02 -0330

    updated and moved db properties

---
+++ src/main/java/Assignments/DBProperties.java
@@ -0,0 +1,18 @@
+package Assignments;
+
+
+
+
+public class DBProperties {
+    public static final String DATABASE_URL = "jdbc:mariadb://localhost:3306";
+
+    public static final String DATABASE_USER = "";
+    //root - school
+
+    public static final String DATABASE_PASSWORD = "";
+    //Password1 - School
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/org/example/JDBC/DBProperties.java
+++ src/main/java/org/example/JDBC/DBProperties.java
@@ -1,11 +1,14 @@
 package org.example.JDBC;
 
+
+
+
 public class DBProperties {
     public static final String DATABASE_URL = "jdbc:mariadb://localhost:3306";
 
-    public static final String DATABASE_USER = "root";
+    public static final String DATABASE_USER = "vsuser";
 
-    public static final String DATABASE_PASSWORD = "Password1";
+    public static final String DATABASE_PASSWORD = "Crunchyt0wn";
 
     public static void main(String[] args) {
 
